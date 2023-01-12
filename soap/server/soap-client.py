67af33cb5fa4d66a74bcaeb3c5db9b68c1c32c9a
Guillaume Schurck <g.schurck@gmail.com>
2023-01-12 22:03:37 +0100

    move api and start soap

---
+++ soap/server/soap-client.py
@@ -0,0 +1,5 @@
+from pysimplesoap.client import SoapClient
+
+wsdl_url = "http://127.0.0.1:8050/"
+c = SoapClient(wsdl=wsdl_url, soap_ns='soap', trace=True)
+print(c.InitiateTransfer(sessionId="sid", msisdn="254722000000", amount="100", language="en"))
