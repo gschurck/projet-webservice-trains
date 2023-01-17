c820e666c3277178907e668abeb8e3fe5b0a040b
Guillaume Schurck <g.schurck@gmail.com>
2023-01-17 23:08:36 +0100

    remove useless soap directories

--- soap/pysimplesoap/client.py
+++
@@ -1,4 +0,0 @@
-from test import InitiateTransfer, TransServerServiceStub
-
-i = InitiateTransfer(sessionId="sid", msisdn="254722000000", amount="100", language="en")
-TransServerServiceStub().InitiateTransfer(i)
